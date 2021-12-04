37ce46162a576dedaa604698043302488aa8612d
LsMacox <zugeraev2@gmail.com>
2021-12-04 16:38:17 +0300

    [modified] init.sh

--- init.sh
+++ init.sh
@@ -11,6 +11,16 @@ export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 DIR_BACKEND='asknt-laravel-back-hpace'
 DIR_FRONTEND='asknt-vue-front-hpace'
 
+if [ -d "$DIR_BACKEND" ] && [ -d "$DIR_FRONTEND" ];
+then
+  docker-compose down
+  docker-compose up -d nginx postgres redis
+  docker-compose exec workspace bash -c 'cd /var/www/'"$DIR_BACKEND"' && chmod +x build.sh && ./build.sh'
+  docker-compose exec workspace bash -c 'cd /var/www/'"$DIR_FRONTEND"' && chmod +x build.sh && ./build.sh'
+  echo 'Initializing successful'
+  exit 0
+fi
+
 if [ ! -d "$DIR_BACKEND" ];
 then
     git clone https://"$GIT_USERNAME":"$GIT_OAUTH_TOKEN"@github.com/HPACE-IT-development/asknt-laravel-back-hpace.git
@@ -24,15 +34,5 @@ then
     cd asknt-vue-front-hpace || exit
     cp .env.example .env
     cd ..
-fi
-
-if [ -d "$DIR_BACKEND" ] && [ -d "$DIR_FRONTEND" ];
-then
-  docker-compose down
-  docker-compose up -d nginx postgres redis
-  docker-compose exec workspace bash -c 'cd /var/www/'"$DIR_BACKEND"' && chmod +x build.sh && ./build.sh'
-  docker-compose exec workspace bash -c 'cd /var/www/'"$DIR_FRONTEND"' && chmod +x build.sh && ./build.sh'
-  echo 'Initializing successful'
-  exit 0
 fi
 
